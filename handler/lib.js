c3ab092d76b04ac4dd22bcd7784567df7ba4c871
imankitkumar <67103569+imankitkumar@users.noreply.github.com>
2020-09-06 16:18:15 +0530

    Update lib.js

--- handler/lib.js
+++ handler/lib.js
@@ -3,14 +3,16 @@ const fetch = require('got');
 
 const data = async (url) => {
 
-  const ap = await fetch(`${url}?__a=1`);
+  const response = await fetch(`${url}?__a=1`);
 
-  const json = JSON.parse(ap.body);
+  const json = JSON.parse(response.body);
 
   return {
+
     "pic_url": `${json.graphql.shortcode_media.display_url}&dl=1`,
     "video_link": `${json.graphql.shortcode_media.video_url}&dl=1`
-  };
+  
+};
 
 };
 
